97102e0460721edc12c97e176d1a8916b7d868c9
yevhenMedovnyk <yevhenmed@gmail.com>
2023-08-29 14:05:33 +0300

    init: start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import './App.css';
+import {Route, Routes} from 'react-router';
+import './App.scss';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home/Home';
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
