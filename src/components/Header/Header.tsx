4380484f8d3ed25dff8dee6a679d1e82ea304076
Hanna <hanna.horbova@gmail.com>
2023-09-05 17:18:24 +0300

    fix:add props styles to button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import Button from '../Button/Button';
-import iconLogIn from 'src/images/log-in-icon.svg';
+// import Button from '../Button/Button';
+// import iconLogIn from 'src/images/log-in-icon.svg';
+// import iconLogOut from 'src/images/log-out-icon.svg';
+// import burgerMenuOpenIcon from 'src/images/burger-menu-open-icon.svg';
+// import burgerMenuCloseIcon from 'src/images/burger-menu-close-icon.svg';
 import Logotype from '../Logotype/Logotype';
 
 const Header: React.FC = () => {
@@ -8,20 +11,50 @@ const Header: React.FC = () => {
 		<div>
 			Header
 			<Logotype />
+			{/* <Button
+				buttonClasses={'primaryTextOnlyButton'}
+
+				type={'button'}
+				name={'Допомогти'}
+				onClick={() => console.log('Клік Допомогти')}
+			/>
+
+
 			<Button
+				buttonClasses={'secondaryIconLeftButton'}
+				divClasses={'miniIconContainer'}
+				imgClasses={'miniIconContainer'}
 				type={'button'}
 				name={'Вхід'}
 				imgPath={iconLogIn}
-				disabled={false}
-				onClick={() => console.log('Enter')}
+				onClick={() => console.log('Клік Вхід')}
 			/>
 			<Button
+				buttonClasses={'secondaryIconLeftButton'}
+				divClasses={'miniIconContainer'}
+				imgClasses={'miniIconContainer'}
 				type={'button'}
-				name={'Вхід'}
-				imgPath={iconLogIn}
-				disabled={false}
-				onClick={() => console.log('Enter')}
+				name={'Вихід'}
+				imgPath={iconLogOut}
+				onClick={() => console.log('Клік Вихід')}
 			/>
+
+			<Button
+				buttonClasses={'tertiaryIconOnlyButton'}
+				divClasses={'bigIconContainer'}
+				imgClasses={'bigIcon'}
+				type={'button'}
+				imgPath={burgerMenuOpenIcon}
+				onClick={() => console.log('Клік Відкрити меню')}
+			/>
+			<Button
+				buttonClasses={'tertiaryIconOnlyButton'}
+				divClasses={'bigIconContainer'}
+				imgClasses={'bigIcon'}
+				type={'button'}
+				imgPath={burgerMenuCloseIcon}
+				onClick={() => console.log('Клік Закрити меню')}
+			/> */}
 		</div>
 	);
 };
