c82ee4fe42b37be46921e1e3e121cbeeb4200830
Nataliya <78379183+pylkonata@users.noreply.github.com>
2023-09-07 18:05:06 +0300

    feat: add header jsx, styles, refactor button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import s from './Button.module.scss';
+import clsx from 'clsx';
 
 interface IButtonProps {
 	name?: string;
@@ -23,13 +24,13 @@ const Button: React.FC<IButtonProps> = ({
 	type,
 	disabled,
 }) => {
+	const btnClasses =
+		buttonClasses && buttonClasses.length > 0
+			? clsx(s.button, buttonClasses?.split(' ').map((item) => s[item]))
+			: s.button;
+
 	return (
-		<button
-			className={[s.button, buttonClasses && s[buttonClasses]].join(' ')}
-			disabled={disabled}
-			type={type}
-			onClick={onClick}
-		>
+		<button className={btnClasses} disabled={disabled} type={type} onClick={onClick}>
 			<div className={[divClasses && s[divClasses]].join(' ')}>
 				{imgPath && <img className={[imgClasses && s[imgClasses]].join(' ')} src={imgPath} />}
 			</div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,63 +1,86 @@
-import React from 'react';
-import Logotype from '../Logotype/Logotype';
-// import Button from '../Button/Button';
-// import iconLogIn from 'src/images/log-in-icon.svg';
-// import iconLogOut from 'src/images/log-out-icon.svg';
-// import burgerMenuOpenIcon from 'src/images/burger-menu-open-icon.svg';
-// import burgerMenuCloseIcon from 'src/images/burger-menu-close-icon.svg';
+import { useState } from 'react';
 
-// import s from './Header.module.scss';
+import Logotype from 'src/components/Logotype/Logotype';
+import Menu from 'src/components/Menu/Menu';
+import Button from 'src/components/Button/Button';
+
+import s from './Header.module.scss';
+
+import logIn from 'src/assets/icons/header/log-in-icon.svg';
+import logOut from 'src/assets/icons/header/log-out-icon.svg';
+import burgerMenuOpen from 'src/assets/icons/header/burger-menu-open-icon.svg';
+import burgerMenuClose from 'src/assets/icons/header/burger-menu-close-icon.svg';
 
 const Header: React.FC = () => {
+	const [isLogin, setIsLogin] = useState(false);
+	const [isOpenBurgerMenu, setIsOpenBurgerMenu] = useState(false);
+	const handleIsLogin = () => {
+		setIsLogin((prev) => !prev);
+	};
+
+	const handleIsOpenBurgerMenu = () => {
+		setIsOpenBurgerMenu((prev) => !prev);
+	};
+
+	const authBtn = isLogin ? (
+		<Button
+			buttonClasses={'secondaryIconLeftButton'}
+			divClasses={'miniIconContainer'}
+			imgClasses={'miniIconContainer'}
+			type={'button'}
+			name={'Вихід'}
+			imgPath={logOut}
+			onClick={handleIsLogin}
+		/>
+	) : (
+		<Button
+			buttonClasses={'secondaryIconLeftButton'}
+			divClasses={'miniIconContainer'}
+			imgClasses={'miniIconContainer'}
+			type={'button'}
+			name={'Вхід'}
+			imgPath={logIn}
+			onClick={handleIsLogin}
+		/>
+	);
+
+	const menuBtn = isOpenBurgerMenu ? (
+		<Button
+			buttonClasses={'tertiaryIconOnlyButton burgerMenuBtn'}
+			divClasses={'bigIconContainer'}
+			imgClasses={'bigIcon'}
+			type={'button'}
+			imgPath={burgerMenuClose}
+			onClick={handleIsOpenBurgerMenu}
+		/>
+	) : (
+		<Button
+			buttonClasses={'tertiaryIconOnlyButton burgerMenuBtn'}
+			divClasses={'bigIconContainer'}
+			imgClasses={'bigIcon'}
+			type={'button'}
+			imgPath={burgerMenuOpen}
+			onClick={handleIsOpenBurgerMenu}
+		/>
+	);
+
 	return (
-		<div>
-			Header
-			<Logotype />
-			{/* <Button
-				buttonClasses={'primaryTextOnlyButton'}
-
-				type={'button'}
-				name={'Допомогти'}
-				onClick={() => console.log('Клік Допомогти')}
-			/>
-
-
-			<Button
-				buttonClasses={'secondaryIconLeftButton'}
-				divClasses={'miniIconContainer'}
-				imgClasses={'miniIconContainer'}
-				type={'button'}
-				name={'Вхід'}
-				imgPath={iconLogIn}
-				onClick={() => console.log('Клік Вхід')}
-			/>
-			<Button
-				buttonClasses={'secondaryIconLeftButton'}
-				divClasses={'miniIconContainer'}
-				imgClasses={'miniIconContainer'}
-				type={'button'}
-				name={'Вихід'}
-				imgPath={iconLogOut}
-				onClick={() => console.log('Клік Вихід')}
-			/>
-
-			<Button
-				buttonClasses={'tertiaryIconOnlyButton'}
-				divClasses={'bigIconContainer'}
-				imgClasses={'bigIcon'}
-				type={'button'}
-				imgPath={burgerMenuOpenIcon}
-				onClick={() => console.log('Клік Відкрити меню')}
-			/>
-			<Button
-				buttonClasses={'tertiaryIconOnlyButton'}
-				divClasses={'bigIconContainer'}
-				imgClasses={'bigIcon'}
-				type={'button'}
-				imgPath={burgerMenuCloseIcon}
-				onClick={() => console.log('Клік Закрити меню')}
-			/> */}
-		</div>
+		<header className={s.header}>
+			<div className={s.container}>
+				<Logotype />
+				<Menu />
+				<div className={s.btnWrapper}>
+					<Button
+						buttonClasses={'primaryTextOnlyButton helpBtn'}
+						type={'button'}
+						name={'Допомогти'}
+						onClick={() => console.log('Клік Допомогти')}
+					/>
+					{authBtn}
+				</div>
+				{menuBtn}
+			</div>
+		</header>
 	);
 };
 
