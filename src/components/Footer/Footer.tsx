710a0c1ae689e862cb57ab430803fea20fcbe375
Hanna <hanna.horbova@gmail.com>
2023-09-14 20:20:19 +0300

    feat: add icons for footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import Logotype from 'src/components/Logotype/Logotype';
+import Contacts from 'src/components/Contacts/Contacts';
+
+import s from 'src/components/Footer/Footer.module.scss';
+import FooterNavMenu from 'src/components/FooterNavMenu/FooterNavMenu';
 
 const Footer: React.FC = () => {
-    return <div>Footer</div>;
+	return (
+		<footer>
+			<div className={s.container}>
+				<div className={s.mainContentContainer}>
+					<Logotype />
+
+					<div className={s.navContactsContainer}>
+						<FooterNavMenu documents={true} navClass={'footerMenu'} />
+
+						<Contacts />
+					</div>
+				</div>
+
+				<div className={s.rights}>Розробка Baza Trainee Ukraine 2023 © Усі права захищені</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
