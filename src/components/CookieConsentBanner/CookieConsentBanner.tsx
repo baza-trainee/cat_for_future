c6f0132d1e763089032f8cc56d17bdd91a9e9abb
arybary <l3fljl5ldydy@gmail.com>
2023-09-14 15:11:48 +0300

    refactor: add btn style, add logic cklick for localstorage

--- src/components/CookieConsentBanner/CookieConsentBanner.tsx
+++ src/components/CookieConsentBanner/CookieConsentBanner.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import s from './CookieConsentBanner.module.scss';
-import Button from '../Button/Button';
 import close from 'src/assets/icons/close.svg';
 
 const CookieConsentBanner: React.FC = () => {
 	const [showBanner, setShowBanner] = useState<boolean>(true);
 
-	const handleAccept = () => {
+	const hasAcceptedCookies = localStorage.getItem('acceptedCookies');
+
+	useEffect(() => {
+		if (hasAcceptedCookies) {
+			setShowBanner(false);
+		}
+	}, [hasAcceptedCookies]);
+
+	const acceptCookies = () => {
+		localStorage.setItem('acceptedCookies', 'true');
+
 		setShowBanner(false);
 	};
 
-	if (!showBanner) {
-		return null;
-	}
+	const closeBanner = () => {
+		setShowBanner(false);
+	};
+
 	return (
-		<div className={s.cookie}>
-			<div className={s.close}>
-				<img onClick={handleAccept} src={close} alt="Close" />
+		showBanner && (
+			<div className={s.cookie}>
+				<div className={s.closeWrapper}>
+					<img className={s.close} onClick={closeBanner} src={close} alt="Close" />
+				</div>
+				<div className={s.text}>
+					<p>
+						Цей сайт використовує файли cookies для правильної роботи та покращення сервісу. Якщо ви
+						погоджуєтесь з їх використанням, натисніть OK. Більше інформації в
+					</p>
+					<a className={s.link}>Політика конфіденційності</a>
+				</div>
+				<div className={s.btnWrapper}>
+					<button className={s.btn} onClick={acceptCookies}>
+						OK
+					</button>
+				</div>
 			</div>
-			<p>
-				Цей сайт використовує файли cookies для правильної роботи та покращення сервісу. Якщо ви
-				погоджуєтесь з їх використанням, натисніть OK. Більше інформації в Політика конфіденційності
-			</p>
-			<a className={s.link}>Політика конфіденційності</a>
-			<Button
-				buttonClasses={'primaryBtn helpBtn'}
-				type={'button'}
-				name={'OK'}
-				onClick={handleAccept}
-			/>
-		</div>
+		)
 	);
 };
 
