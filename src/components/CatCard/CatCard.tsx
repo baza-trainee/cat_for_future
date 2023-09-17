c7baec759521fafefd2597d05f27344a931e6940
Yevhen Medovnyk <yevhenmed@gmail.com>
2023-09-17 23:22:42 +0300

    feat: add favorite btn, refactor btns

--- src/components/CatCard/CatCard.tsx
+++ src/components/CatCard/CatCard.tsx
@@ -5,11 +5,19 @@ import s from './CatCard.module.scss';
 import cat from 'src/assets/images/cats/cat_1.jpg';
 import lockIcon from 'src/assets/icons/cat_card/lock.svg';
 //import homeIcon from 'src/assets/icons/cat_card/home.svg';
-import heartIcon from 'src/assets/icons/cat_card/heart.svg';
+import {ReactComponent as HeartIcon} from 'src/assets/icons/cat_card/heart.svg';
 
 const btnStyle = {
 	width: '100%',
 	gap: '.37rem',
+};
+const favoriteBtnStyle = {
+	backgroundColor: 'rgba(255, 255, 255, 0.9)',
+	width: '2.875rem',
+	height: '2.875rem',
+	borderRadius: '50%',
+	padding: ".75rem",
+	cursor: 'pointer'
 };
 
 const CatCard: React.FC = () => {
@@ -17,6 +25,14 @@ const CatCard: React.FC = () => {
 		<div className={s.wrapper}>
 			<div className={s.image}>
 				<img src={cat} alt="cat_name" />
+				<div className={s.favoriteBtnContainer}>
+					<Button
+						buttonClasses={'primaryBtn'}
+						onClick={() => console.log('to favorite')}
+						styleBtn={favoriteBtnStyle}
+						children={<HeartIcon className={s.heartFavoriteBtn} />}
+					/>
+				</div>
 			</div>
 			<div className={s.content}>
 				<div className={s.header}>
@@ -33,12 +49,10 @@ const CatCard: React.FC = () => {
 			<div className={s.buttonContainer}>
 				<Button
 					buttonClasses={'primaryBtn'}
-					imgPath={heartIcon}
-					type="button"
 					name="Забронювати"
 					onClick={() => console.log('Клік Забронювати')}
-					imgClasses={'catCardIcon'}
 					styleBtn={btnStyle}
+					children={<HeartIcon className={s.heartIconBtn} />}
 				/>
 			</div>
 		</div>
