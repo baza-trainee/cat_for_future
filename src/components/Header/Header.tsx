323be79492df06db7ead04866bddcc438509ebe5
Nataliya <78379183+pylkonata@users.noreply.github.com>
2023-09-19 15:47:08 +0300

    feat: add navigate to cabinet by route

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,39 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router';
 
 import Logotype from 'src/components/Logotype/Logotype';
 import Menu from 'src/components/Menu/Menu';
 import Button from 'src/components/Button/Button';
+import BurgerMenu from 'src/components/Header/BurgerMenu/BurgerMenu';
 
 import s from './Header.module.scss';
 
 import { ReactComponent as Login } from 'src/assets/icons/header/log-in-icon.svg';
 import { ReactComponent as BurgerMenuOpen } from 'src/assets/icons/header/burger-menu-open-icon.svg';
 import { ReactComponent as BurgerMenuClose } from 'src/assets/icons/header/burger-menu-close-icon.svg';
-import BurgerMenu from 'src/components/Header/BurgerMenu/BurgerMenu';
 
 interface HeaderProps {
 	onOpenModalDonate: () => void;
 	onOpenLoginWindow: (boolean: boolean) => void;
 }
 const Header: React.FC<HeaderProps> = ({ onOpenModalDonate, onOpenLoginWindow }) => {
-	const [isLogin, setIsLogin] = useState<boolean>(false);
 	const [isOpenBurgerMenu, setIsOpenBurgerMenu] = useState<boolean>(false);
 
+	//TODO: insert state isLogin from context or redux
+	const isLogin: boolean = false;
+
+	const navigate = useNavigate();
+
 	const handleIsLogin = () => {
-		setIsLogin((prev) => !prev);
 		!isLogin ? onOpenLoginWindow(true) : null;
 	};
 
 	const handleIsOpenBurgerMenu = () => {
 		setIsOpenBurgerMenu((prev) => !prev);
+	};
+
+	const handleNavigateToCabinet = () => {
+		navigate('/cabinet');
 	};
 
 	return (
@@ -46,7 +54,7 @@ const Header: React.FC<HeaderProps> = ({ onOpenModalDonate, onOpenLoginWindow })
 							type={'button'}
 							name={isLogin ? 'Кабінет' : 'Вхід'}
 							children={<Login className={s.icon} />}
-							onClick={handleIsLogin}
+							onClick={isLogin ? handleNavigateToCabinet : handleIsLogin}
 						/>
 					</div>
 					<Button
