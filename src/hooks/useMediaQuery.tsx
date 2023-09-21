abf86404818e7f8f956905114d48f859209c8888
Yevhen Medovnyk <yevhenmed@gmail.com>
2023-09-21 21:38:02 +0300

    refactor: delete isTablet query

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -1,11 +1,9 @@
 import { useMediaQuery as useQuery } from 'react-responsive';
 
 export const useMediaQuery = () => {
-	const isMobile = useQuery({ query: '(min-width: 320px)' });
 	const isTablet = useQuery({ query: '(min-width: 768px)' });
 	const isDesktop = useQuery({ query: '(min-width: 1280px)' });
 	return {
-		isMobile,
 		isTablet,
 		isDesktop,
 	};
