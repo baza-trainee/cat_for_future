1ec81077f826738dc5c8efcac711b2b08672ef0b
Ivan <vanyok.korobka@ukr.net>
2023-09-22 13:48:55 +0300

    feat/add mobile styles

--- src/components/AdoptKittenInstruction/AdoptKittenInstruction.tsx
+++ src/components/AdoptKittenInstruction/AdoptKittenInstruction.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import Button from '../Button/Button';
+import paws from 'src/assets/icons/adopt-kitten-instruct-paw.svg';
 import s from './AdoptKittenInstruction.module.scss';
 
 const AdoptKittenInstruction: FC = () => {
@@ -9,7 +10,7 @@ const AdoptKittenInstruction: FC = () => {
 
 			<div className={s.instructionList}>
 				<div className={s.instructionItem}>
-					<img src="" alt="" />
+					<img src={paws} alt="Paws" className={s.paws} />
 					<div className={s.descriptionBody}>
 						<h3 className={s.subtitle}>Ознайомтеся з нашими кошенятами</h3>
 						<p className={s.description}>
@@ -20,23 +21,23 @@ const AdoptKittenInstruction: FC = () => {
 				</div>
 
 				<div className={s.instructionItem}>
-					<img src="" alt="" />
+					<img src={paws} alt="Paws" className={s.paws} />
 					<div className={s.descriptionBody}>
-						<h3 className={s.subtitle}>Ознайомтеся з нашими кошенятами</h3>
+						<h3 className={s.subtitle}>Заповніть заявку на укотовлення</h3>
 						<p className={s.description}>
-							Перш за все, ознайомтеся зі списком доступних кошенят на нашому cайті. Ви знайдете
-							фотографії та описи кожного кошеня, які допоможуть вам обрати собі маленького друга.
+							Знайшли кошеня, яке вам подобається? Зареєструйтесь на нашому сайті, заповніть заявку
+							на усиновлення та очікуйте поки воно підросте та його можна буде забрати додому
 						</p>
 					</div>
 				</div>
 
 				<div className={s.instructionItem}>
-					<img src="" alt="" />
+					<img src={paws} alt="Paws" className={s.paws} />
 					<div className={s.descriptionBody}>
-						<h3 className={s.subtitle}>Ознайомтеся з нашими кошенятами</h3>
+						<h3 className={s.subtitle}>Дочекайтесь поки кошеня підросте</h3>
 						<p className={s.description}>
-							Перш за все, ознайомтеся зі списком доступних кошенят на нашому cайті. Ви знайдете
-							фотографії та описи кожного кошеня, які допоможуть вам обрати собі маленького друга.
+							Вже забронювали улюбленця? У власному кабінеті після бронювання кошеняти, ви маєте
+							можливість віслідковувати як він росте та рахувати час до зустрічі
 						</p>
 					</div>
 				</div>
