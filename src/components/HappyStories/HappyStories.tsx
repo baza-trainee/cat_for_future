f56c44b34e531f87a89eba5fd7129872ad17e11b
Nataliya <78379183+pylkonata@users.noreply.github.com>
2023-09-26 00:03:13 +0300

    feat: add logic, styles for expand collapse text

--- src/components/HappyStories/HappyStories.tsx
+++ src/components/HappyStories/HappyStories.tsx
@@ -7,7 +7,7 @@ import StoryCard from 'src/components/HappyStories/StoryCard/StoryCard';
 
 const HappyStories = () => {
 	return (
-		<section className={s.happyStories}>
+		<section className={s.happyStories} id="happyStories">
 			<div className={s.container}>
 				<h2 className={s.title}>Щасливі історіі</h2>
 				<div className={s.storiesWrap}>
--- src/components/HappyStories/StoryCard/StoryCard.tsx
+++ src/components/HappyStories/StoryCard/StoryCard.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { useState } from 'react';
+import clsx from 'clsx';
+
 import s from './StoryCard.module.scss';
 
 import { IStory } from 'src/data/stories.temp';
@@ -5,15 +9,21 @@ import Button from 'src/components/Button/Button';
 import { ReactComponent as ArrayRight } from 'src/assets/icons/arrow-right.svg';
 
 const StoryCard = ({ imgSrc, title, text }: IStory) => {
+	const [isCollapsedText, setIsCollapsedText] = useState(true);
+
+	const handleExpandText = () => {
+		setIsCollapsedText((prev) => !prev);
+	};
+
 	return (
 		<div className={s.card}>
 			<img src={imgSrc} alt={title} className={s.img} />
 			<div className="cardBody">
 				<h3 className={s.title}>{title}</h3>
-				<p className={s.description}>{text}</p>
+				<p className={clsx(s.description, isCollapsedText && s.textCollapsed)}>{text}</p>
 				<div className={s.btnWrap}>
-					<button type="button" className={s.btn}>
-						Читати далі
+					<button type="button" className={s.btn} onClick={handleExpandText}>
+						{isCollapsedText ? 'Читати далі' : 'Згорнути'}
 					</button>
 					<Button
 						buttonClasses={'secondaryBtn  secondaryIconRight'}
