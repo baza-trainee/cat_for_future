23acdcb5c3308f53f26edf17c11114534975b1e0
Nataliya <78379183+pylkonata@users.noreply.github.com>
2023-09-26 21:42:33 +0300

    feat: add jsx, styles for site adaptability

--- src/components/HappyStories/StoryCard/StoryCard.tsx
+++ src/components/HappyStories/StoryCard/StoryCard.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
 import { useState } from 'react';
+import useMediaQuery from 'src/hooks/useMediaQuery';
 import clsx from 'clsx';
 
 import s from './StoryCard.module.scss';
@@ -7,9 +7,11 @@ import s from './StoryCard.module.scss';
 import { IStory } from 'src/data/stories.temp';
 import Button from 'src/components/Button/Button';
 import { ReactComponent as ArrayRight } from 'src/assets/icons/arrow-right.svg';
+import { scrollToSection } from 'src/utils/scrollToSection';
 
 const StoryCard = ({ imgSrc, title, text }: IStory) => {
 	const [isCollapsedText, setIsCollapsedText] = useState(true);
+	const { isDesktop } = useMediaQuery();
 
 	const handleExpandText = () => {
 		setIsCollapsedText((prev) => !prev);
@@ -18,22 +20,24 @@ const StoryCard = ({ imgSrc, title, text }: IStory) => {
 	return (
 		<div className={s.card}>
 			<img src={imgSrc} alt={title} className={s.img} />
-			<div className="cardBody">
+			<div className={s.cardBody}>
 				<h3 className={s.title}>{title}</h3>
 				<p className={clsx(s.description, isCollapsedText && s.textCollapsed)}>{text}</p>
 				<div className={s.btnWrap}>
 					<button type="button" className={s.btn} onClick={handleExpandText}>
 						{isCollapsedText ? 'Читати далі' : 'Згорнути'}
 					</button>
-					<Button
-						buttonClasses={'secondaryBtn  secondaryIconRight'}
-						type={'button'}
-						name="Обери кота для піклування"
-						onClick={() => console.log('click')}
-						styleBtn={{ border: 'none' }}
-					>
-						<ArrayRight className={s.icon} />
-					</Button>
+					{isDesktop && (
+						<Button
+							buttonClasses={'secondaryBtn  secondaryIconRight'}
+							type={'button'}
+							name="Обери кота для піклування"
+							onClick={() => scrollToSection('ourCats')}
+							styleBtn={{ border: 'none', paddingTop: '1rem', paddingBottom: '1rem' }}
+						>
+							<ArrayRight className={s.icon} />
+						</Button>
+					)}
 				</div>
 			</div>
 		</div>
