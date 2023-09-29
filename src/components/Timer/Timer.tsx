27083e376926de85e4a8ff33e372ffa8a6028d2f
Ivan <vanyok.korobka@ukr.net>
2023-09-29 03:11:36 +0300

    feat/modificate Timer className of main tag

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,16 +1,18 @@
 import { FC } from 'react';
 import { pluralize } from 'src/utils/pluralize';
+import clsx from 'clsx';
 import s from './Timer.module.scss';
 
 interface TimerProps {
 	arrCorrectDate: string[];
+	className?: string;
 }
 
-const Timer: FC<TimerProps> = ({ arrCorrectDate }) => {
+const Timer: FC<TimerProps> = ({ arrCorrectDate, className }) => {
 	const [correctDays, correctHours, correctMinutes] = arrCorrectDate;
 
 	return (
-		<div className={s.timerBlock}>
+		<div className={clsx(s.timerBlockForMyKitten, className && s[className])}>
 			<h3 className={s.timerTitle}>Я поїду додому через</h3>
 			<div className={s.timerWrapper}>
 				<div className={s.timerUnit}>
