5420897709d574b341fede829e75b83fe441543a
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2023-10-13 00:54:19 +0300

    feat/32-exit-remove-bugs-35-36-fix

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -93,6 +93,38 @@ const Login: FC = () => {
 		}
 	};
 
+	// validation login form.
+	const { handleSubmit, handleBlur, handleChange, values, errors, touched, isSubmitting } =
+		useFormik({
+			initialValues: {
+				loginEmail: '',
+				loginPassword: '',
+			},
+			validationSchema: Yup.object().shape({
+				loginEmail: Yup.string()
+					.email('Введіть коректну e-mail адресу')
+					.matches(
+						/^[A-Z0-9_%+-]+(\.[A-Z0-9_%+-]+)*@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+						'Введіть коректну e-mail адресу'
+					)
+					.required("Обов'язкове поле"),
+				loginPassword: Yup.string()
+					.min(8, 'Введіть коректний пароль')
+					.max(15, 'Введіть коректний пароль')
+					.matches(
+						/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,15}$/,
+						'Введіть коректний пароль'
+					)
+					.required("Обов'язкове поле"),
+			}),
+			onSubmit: (_, actions) => {
+				setAuthEmailError(false);
+				setAuthPasswordError(false);
+				console.log('Sing IN');
+				actions.resetForm();
+			},
+		});
+
 	useEffect(() => {
 		values.loginEmail && authEmailError
 			? setAuthEmailError(false)
