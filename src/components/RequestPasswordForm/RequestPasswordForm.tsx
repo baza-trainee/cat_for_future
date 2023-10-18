d8ea076bcddcb8dc7cf1891d387f13efa5b65f7a
Nataliya <78379183+pylkonata@users.noreply.github.com>
2023-10-18 23:38:32 +0300

    feat: add response text to request password form

--- src/components/RequestPasswordForm/RequestPasswordForm.tsx
+++ src/components/RequestPasswordForm/RequestPasswordForm.tsx
@@ -1,4 +1,4 @@
-import { FormikHelpers, useFormik } from 'formik';
+import { useFormik } from 'formik';
 import { useNavigate } from 'react-router';
 
 import s from './RequestPasswordForm.module.scss';
@@ -7,18 +7,26 @@ import ModalBack from 'src/components/ModalBack/ModalBack';
 import Button from 'src/components/Button/Button';
 import { requestPasswSchema } from 'src/schemas/requestPassword.schema';
 import clsx from 'clsx';
+import { useState } from 'react';
 
 interface InitValuesRequestPassw {
 	email: string;
 }
 
 const btnStyle = { width: '100%' };
+const subtitleText = {
+	textStart1: 'Введіть email, пов’язаний з вашим акаунтом.',
+	textStart2: 'Якщо у вас є акаунт, вам на email буде надіслано посилання для відновлення пароля',
+	textResponse:
+		'Незабаром на вказану адресу  має надійти лист для зміни пароля. Якщо ви його не бачите, будь ласка, перевірте папку “Спам”.',
+};
 
 const initialValues: InitValuesRequestPassw = {
 	email: '',
 };
 
 const RequestPasswordForm = () => {
+	const [isSuccessResponse, setIsSuccessResponse] = useState<boolean>(false);
 	const navigate = useNavigate();
 
 	const handleCloseForm = () => {
@@ -28,10 +36,11 @@ const RequestPasswordForm = () => {
 		enableReinitialize: true,
 		initialValues: initialValues,
 		validationSchema: requestPasswSchema,
-		onSubmit: (values: InitValuesRequestPassw, actions: FormikHelpers<InitValuesRequestPassw>) => {
-			console.log(values);
-			actions.resetForm();
-			handleCloseForm();
+		onSubmit: () => {
+			//! Update with request to backend
+			setTimeout(() => {
+				setIsSuccessResponse(true);
+			}, 500);
 		},
 	});
 
@@ -39,11 +48,15 @@ const RequestPasswordForm = () => {
 		<ModalBack handleCloseModal={handleCloseForm}>
 			<div className={s.formWrap}>
 				<h2 className={s.title}>Відновлення пароля</h2>
-				<p className={s.subtitle}>
-					<span>Введіть email, пов’язаний з вашим акаунтом.</span>
-					<span>
-						Якщо у вас є акаунт, вам на email буде надіслано посилання для відновлення пароля
-					</span>
+				<p className={clsx(s.subtitle, isSuccessResponse && s.responseText)}>
+					{!isSuccessResponse ? (
+						<>
+							<span>{subtitleText.textStart1}</span>
+							<span>{subtitleText.textStart2}</span>
+						</>
+					) : (
+						subtitleText.textResponse
+					)}
 				</p>
 				<form className={s.form} onSubmit={handleSubmit}>
 					<div className={s.inputWrap}>
@@ -61,13 +74,13 @@ const RequestPasswordForm = () => {
 									values.email && s.active,
 									errors.email && touched.email && s.error
 								)}
-								placeholder="Введіть e-mail"
-								title="E-mail*"
+								placeholder={!isSuccessResponse ? 'Введіть e-mail' : values.email}
 								name="email"
 								id="email"
-								value={values.email}
+								value={!isSuccessResponse ? values.email : ''}
 								onChange={handleChange}
 								onBlur={handleBlur}
+								disabled={isSuccessResponse}
 							/>
 							{errors.email && touched.email && <p className={s.errorMsg}>{errors.email}</p>}
 						</div>
@@ -78,7 +91,7 @@ const RequestPasswordForm = () => {
 						buttonClasses={'primaryBtn'}
 						name={'Надіслати'}
 						type={'submit'}
-						disabled={!isValid || !values.email}
+						disabled={!isValid || !values.email || isSuccessResponse}
 					/>
 				</form>
 			</div>
