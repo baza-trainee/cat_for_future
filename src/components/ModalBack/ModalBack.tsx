1bdde86cdbb3d3d53142ae4cb446640ce167cfdc
Nataliya <78379183+pylkonata@users.noreply.github.com>
2023-10-24 23:33:14 +0300

    refactor: create function close modals by clicking on background

--- src/components/ModalBack/ModalBack.tsx
+++ src/components/ModalBack/ModalBack.tsx
@@ -1,10 +1,11 @@
-import { CSSProperties, FC, MouseEvent } from 'react';
+import { CSSProperties, FC } from 'react';
 
 import s from './ModalBack.module.scss';
 
 import { ReactComponent as Close } from 'src/assets/icons/login-close-btn.svg';
 
 import { useEscape } from 'src/hooks/useEscape';
+import { onClickModalBack } from 'src/utils/onClickModalBack';
 
 interface ModalBackProps {
 	children: React.ReactNode;
@@ -14,14 +15,9 @@ interface ModalBackProps {
 
 const ModalBack: FC<ModalBackProps> = ({ children, handleCloseModal, wrapStyle }) => {
 	useEscape(handleCloseModal);
-	const handleModalClick = (e: MouseEvent<HTMLDivElement>) => {
-		if (e.target === e.currentTarget) {
-			handleCloseModal();
-		}
-	};
 
 	return (
-		<div className={s.backdrop} onClick={handleModalClick}>
+		<div className={s.backdrop} onClick={(e) => onClickModalBack(e, handleCloseModal)}>
 			<div className={s.modalWrapper}>
 				<Close onClick={handleCloseModal} className={s.closeIcon} />
 				<div className={s.insideWrapper} style={wrapStyle}>
