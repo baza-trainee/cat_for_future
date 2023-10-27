f81d68eefcc1cb8e80fbd87b6c70a5354b94ad55
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2023-10-28 02:19:45 +0300

    feat/07-modal-donate-animation-fix

--- src/components/ModalDonate/ModalDonate.tsx
+++ src/components/ModalDonate/ModalDonate.tsx
@@ -5,8 +5,6 @@ import s from './ModalDonate.module.scss';
 import close from 'src/assets/icons/close_white.svg';
 import Button from 'src/components/Button/Button';
 
-import CryptoJS from 'crypto-js';
-
 interface DonateAmount {
 	id: number;
 	amount: string;
@@ -18,6 +16,7 @@ interface ModalProps {
 }
 
 const ModalDonate: React.FC<ModalProps> = ({ onClose, status }) => {
+	const [isVisible, setIsVisible] = useState(status);
 	const [price, setPrice] = useState('');
 	const setValue = (value: string) => {
 		setPrice(value);
@@ -32,13 +31,8 @@ const ModalDonate: React.FC<ModalProps> = ({ onClose, status }) => {
 	const [selectedId, setSelectedId] = useState<number | null>(null);
 
 	useEffect(() => {
-		const escFunction = (e: KeyboardEvent) => {
-			if (e.key === 'Escape') {
-				onClose();
-			}
-		};
-
 		if (status) {
+			setIsVisible(true);
 			document.addEventListener('keyup', escFunction, false);
 			return () => {
 				document.removeEventListener('keyup', escFunction, false);
@@ -46,51 +40,71 @@ const ModalDonate: React.FC<ModalProps> = ({ onClose, status }) => {
 		}
 	}, [status, onClose]);
 
-	const handleModalClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-		if (e.target === e.currentTarget) {
-			onClose();
+	const escFunction = (e: KeyboardEvent) => {
+		if (e.key === 'Escape') {
+			// onClose();
+			setIsVisible(false);
 		}
 	};
 
-	const merchantAccount = import.meta.env.VITE_MERCHANT_ACCOUNT;
-	const merchantDomainName = 'cat-for-future-9qj7.vercel.app';
-	const orderReference = Date.now().toString();
-	const orderDate = Date.now().toString();
-	const amount = price;
-	const currency = 'UAH';
-	const productName = ['Кіт на виріст'];
-	const productCount = ['1'];
-	const productPrice = [price];
-	const message = [
-		merchantAccount,
-		merchantDomainName,
-		orderReference,
-		orderDate,
-		amount,
-		currency,
-		...productName,
-		...productCount,
-		...productPrice,
-	].join(';');
-	const wordArray = CryptoJS.enc.Utf8.parse(message);
-	const hash = CryptoJS.HmacMD5(wordArray, `${import.meta.env.VITE_WAY_FOR_PAY_KEY}`).toString();
+	const handleAnimationEnd = () => {
+		if (!isVisible) {
+			onClose();
+			setIsVisible(false);
+		}
+	};
+
+	const handleModalClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		if (e.target === e.currentTarget) {
+			setIsVisible(false);
+		}
+	};
+
+	// const merchantAccount = import.meta.env.VITE_MERCHANT_ACCOUNT;
+	// const merchantDomainName = 'cat-for-future-9qj7.vercel.app';
+	// const orderReference = Date.now().toString();
+	// const orderDate = Date.now().toString();
+	// const amount = price;
+	// const currency = 'UAH';
+	// const productName = ['Кіт на виріст'];
+	// const productCount = ['1'];
+	// const productPrice = [price];
+	// const message = [
+	// 	merchantAccount,
+	// 	merchantDomainName,
+	// 	orderReference,
+	// 	orderDate,
+	// 	amount,
+	// 	currency,
+	// 	...productName,
+	// 	...productCount,
+	// 	...productPrice,
+	// ].join(';');
+	// const wordArray = CryptoJS.enc.Utf8.parse(message);
+	// const hash = CryptoJS.HmacMD5(wordArray, `${import.meta.env.VITE_WAY_FOR_PAY_KEY}`).toString();
 
 	return (
-		<div className={`${s.backdrop} ${!status ? s.fadeOut : ''}`} onClick={handleModalClick}>
+		// <div className={`${s.backdrop} ${!status ? s.fadeOut : ''}`} onClick={handleModalClick}>
+		<div
+			className={`${s.backdrop} ${isVisible ? s.fadeIn : s.fadeOut}`}
+			onClick={handleModalClick}
+			onAnimationEnd={handleAnimationEnd}
+		>
 			<div className={s.modalWrapper}>
+				{/* <div className={`${s.modalWrapper} ${isVisible ? s.fadeIn : s.fadeOut}`}> */}
 				<div className={s.modalBanner}>
-					<img className={s.close} onClick={onClose} src={close} alt="Close" />
+					<img className={s.close} onClick={handleModalClick} src={close} alt="Close" />
 				</div>
 				<div className={s.textWrapper}>
 					<p>Зібрані кошти йдуть на харчування та медичну допомогу</p>
 				</div>
 				<form
-					method="post"
-					action="https://secure.wayforpay.com/pay"
-					accept-сharset="utf-8"
+					// method="post"
+					// action="https://secure.wayforpay.com/pay"
+					// accept-сharset="utf-8"
 					className={s.formDonate}
 				>
-					<input
+					{/* <input
 						className={s.hiddenInput}
 						name="merchantAccount"
 						value={`${import.meta.env.VITE_MERCHANT_ACCOUNT}`}
@@ -116,7 +130,7 @@ const ModalDonate: React.FC<ModalProps> = ({ onClose, status }) => {
 					/>
 					<input className={s.hiddenInput} name="merchantSignature" value={hash} readOnly />
 					<input className={s.hiddenInput} name="orderReference" value={orderReference} readOnly />
-					<input className={s.hiddenInput} name="orderDate" value={orderDate} readOnly />
+					<input className={s.hiddenInput} name="orderDate" value={orderDate} readOnly /> */}
 
 					<div className={s.donatesAmountWrapper}>
 						{donate.map(({ id, amount }) => (
@@ -154,10 +168,10 @@ const ModalDonate: React.FC<ModalProps> = ({ onClose, status }) => {
 							styleBtn={{ width: '100%' }}
 						/>
 					</div>
-					<input className={s.hiddenInput} name="currency" value="UAH" readOnly />
+					{/* <input className={s.hiddenInput} name="currency" value="UAH" readOnly />
 					<input className={s.hiddenInput} name="productName[]" value="Кіт на виріст" readOnly />
 					<input className={s.hiddenInput} name="productPrice[]" value={price} readOnly />
-					<input className={s.hiddenInput} name="productCount[]" value="1" readOnly />
+					<input className={s.hiddenInput} name="productCount[]" value="1" readOnly /> */}
 				</form>
 			</div>
 		</div>
