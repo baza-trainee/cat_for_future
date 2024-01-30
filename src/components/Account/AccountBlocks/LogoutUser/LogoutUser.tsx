84dfaa935b7ede1ab03346d1277db5ec770ca6b5
Yevhen Medovnyk <yevhenmed@gmail.com>
2024-01-30 15:03:26 +0200

    feat: add logOut-user logic

---
+++ src/components/Account/AccountBlocks/LogoutUser/LogoutUser.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ModalAccount from '../../Modal/ModalAccount';
+import QuestionModalAccount from '../../Modal/QuestionModalAccount';
+import { useLogOutMutation } from 'src/store/slice/authApiSlice';
+import { useNavigate } from 'react-router';
+
+const LogoutUser: React.FC = () => {
+	const [logOut] = useLogOutMutation();
+
+	const navigate = useNavigate();
+
+	const successFnc = async () => {
+		await logOut('');
+		localStorage.removeItem('token');
+		navigate('/');
+	};
+	const declineFnc = () => {
+		navigate(-1);
+	};
+
+	return (
+		<div>
+			<ModalAccount onClose={declineFnc}>
+				<QuestionModalAccount
+					question="Вихід з акаунта"
+					text="Ви впевнені, що хочете вийти з акаунту?"
+					successFnc={successFnc}
+					declineFnc={declineFnc}
+				/>
+			</ModalAccount>
+		</div>
+	);
+};
+
+export default LogoutUser;
--- src/pages/PersonalAccount/PersonalAccount.tsx
+++ src/pages/PersonalAccount/PersonalAccount.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 
 import PersonalAccountLayout from 'src/components/Account/AccountLayout/AccountLayout';
-import AccountLogout from 'src/components/Account/AccountModals/AcoountLogout';
 import AccountDelete from 'src/components/Account/AccountModals/AccountDelete';
 import MyData from 'src/components/Account/AccountBlocks/MyData/MyData';
 import ChangePassword from 'src/components/Account/AccountBlocks/ChangePassword/ChangePassword';
 import MyKitten from 'src/components/Account/AccountBlocks/MyKitten/MyKitten';
 import ProtectedRoute from 'src/routes/ProtectedRouteAdmin.tsx';
+import LogoutUser from 'src/components/Account/AccountBlocks/LogoutUser/LogoutUser';
 
 const PersonalAccount: React.FC = () => (
 	<Routes>
@@ -17,7 +17,7 @@ const PersonalAccount: React.FC = () => (
 				<Route index element={<MyData />} />
 				<Route path="my-kitten" element={<MyKitten />} />
 				<Route path="change-password" element={<ChangePassword />} />
-				<Route path="logout" element={<AccountLogout />} />
+				<Route path="logout" element={<LogoutUser />} />
 				<Route path="delete-account" element={<AccountDelete />} />
 			</Route>
 		</Route>
