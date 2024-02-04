cf2bde5bc5b3dcb1e446bfec2c57f7749495af37
Kateryna Laina <boba4ok@gmail.com>
2024-02-04 11:30:51 +0200

    feat: change registration logic

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -2,6 +2,7 @@ import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { showLogin, showLoginAdmin } from '../store/slice/showLoginSlice';
 import { AppDispatch } from 'src/store/store';
+import { setUserData } from 'src/store/slice/userSlice.ts';
 
 export const useActions = () => {
 	const dispatch = useDispatch<AppDispatch>();
@@ -10,6 +11,7 @@ export const useActions = () => {
 		{
 			showLogin,
 			showLoginAdmin,
+			setUserData,
 		},
 		dispatch
 	);
