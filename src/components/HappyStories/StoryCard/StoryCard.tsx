274b8f93f89c4ddadc30b6f71831aba178f3f6c6
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2024-02-10 03:01:24 +0200

    fix/bug-48-Stories-scroll

--- src/components/HappyStories/StoryCard/StoryCard.tsx
+++ src/components/HappyStories/StoryCard/StoryCard.tsx
@@ -1,46 +1,37 @@
+import { useState } from 'react';
 import useMediaQuery from 'src/hooks/useMediaQuery';
-import clsx from 'clsx';
-
 import s from './StoryCard.module.scss';
-
 import { IStory } from 'src/types/IStory';
 import Button from 'src/components/Button/Button';
 import { ReactComponent as ArrayRight } from 'src/assets/icons/arrow-right.svg';
 import { scrollToSection } from 'src/utils/scrollToSection';
-import { useScroll } from 'src/hooks/useScroll.ts';
 
 interface StoryCardProps extends IStory {
 	i: number;
 	handleChangeTextState: (i: number) => void;
 	isCollapsedText?: boolean;
 }
-const StoryCard = ({
-	i,
-	media_path,
-	title,
-	text,
-	handleChangeTextState,
-	isCollapsedText,
-}: StoryCardProps) => {
+const StoryCard = ({ media_path, title, text }: StoryCardProps) => {
 	const { isDesktop, isTablet } = useMediaQuery();
-	const { executeScroll } = useScroll();
 
 	const collapseTextHandler = () => {
-		handleChangeTextState(i);
-		if (!isCollapsedText && !isTablet && !isDesktop) {
-			executeScroll('happyStories');
+		setIsOpen(!isOpen);
+		if (isOpen && !isTablet && !isDesktop) {
+			scrollToSection('happyStories');
 		}
 	};
+
+	const [isOpen, setIsOpen] = useState(false);
 
 	return (
 		<div className={s.card}>
 			<img src={media_path} alt={title} className={s.img} />
 			<div className={s.cardBody}>
 				<h3 className={s.title}>{title}</h3>
-				<p className={clsx(s.description, isCollapsedText && s.textCollapsed)}>{text}</p>
+				<p className={!isOpen ? s.textCollapsed : s.description}>{text}</p>
 				<div className={s.btnWrap}>
 					<button type="button" className={s.btn} onClick={collapseTextHandler}>
-						{isCollapsedText ? 'Читати далі' : 'Згорнути'}
+						{!isOpen ? 'Читати далі' : 'Згорнути'}
 					</button>
 					{isDesktop && (
 						<Button
